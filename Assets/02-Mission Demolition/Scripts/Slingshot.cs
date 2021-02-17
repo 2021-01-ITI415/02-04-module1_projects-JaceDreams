479667da8debf61708c87f77daaf01936a963456
JaceDreams <jacedreams@gmail.com>
2021-02-17 16:03:37 -0500

    02 Creating the Slingshot Class

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
